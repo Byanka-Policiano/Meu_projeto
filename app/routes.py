790df2339f6c34ba87db57eef9014165bff22f3e
Byanka Policiano Santos <policianobyanka@gmail.com>
2023-08-07 11:25:53 -0300

    adicionei formularios

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template
-
+from app.forms import contato
 
 @app.route('/')
 def index():
@@ -8,8 +8,26 @@ def index():
 
 @app.route('/contatos')
 def contatos():
+    dados_formulario = None
+    formulario = contato()
+    if formulario.validate_on_submit():
+        nome = formulario.nome.data
+        email = formulario.email.data
+        telefone = formulario.telefone.data
+        mensagem = formulario.mensagem.data
+
+        dados_formulario = {
+            'nome': nome,
+            'email': email,
+            'telefone': telefone,
+            'mensagem': mensagem
+        }
     return render_template('contatos.html',titulo = 'Contatos')
 
 @app.route('/sobre')
 def sobre():
     return render_template('sobre.html',titulo = 'Sobre')
+
+@app.route('/produtos')
+def produtos():
+    return render_template('produto.html',titulo = 'Produtos')
