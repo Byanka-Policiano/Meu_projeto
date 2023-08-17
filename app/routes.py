aa1e93673b463c08bf9462d98f30027676b707b4
Byanka Policiano Santos <policianobyanka@gmail.com>
2023-08-17 11:37:18 -0300

    17/08/2023-11:37

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from app import app, db
-from flask import render_template, url_for, request, flash
+from flask import render_template, url_for, request, flash, session, redirect
 from app.forms import Contato
 from app.models import ContatoModel
+from app.forms import Cadastro
+from app.models import CadastroModel
+import time 
 
 
 @app.route('/')
@@ -25,6 +28,42 @@ def contatos():
         db.session.commit()
     return render_template('contatos.html', titulo = 'Contatos',formulario = formulario,dados_formulario = dados_formulario)
 
+
+@app.route('/cadastro', methods=['POST', 'GET'])
+def cadastro():
+    cadastro = Cadastro()
+    print('Acessou a rota de cadastro')
+    if cadastro.validate_on_submit():
+        flash('Seu cadastro foi realizado com sucesso!')
+
+        nome = cadastro.nome.data 
+        sobrenome = cadastro.sobrenome.data
+        email = cadastro.email.data
+        senha = cadastro.senha.data
+
+        novo_cadastro = CadastroModel(nome=nome, sobrenome=sobrenome, email=email,  senha=senha)
+        db.session.add(novo_cadastro)
+        db.session.commit()
+
+
+    return render_template('cadastro.html', titulo = 'Cadastro', cadastro=cadastro)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email').lower()
+        senha = request.form.get('senha')
+
+        usuario = CadastroModel.query.filter_by(email = email, senha = senha).first()
+        if usuario and usuario.senha == senha:
+            session['email'] = usuario.id
+            time.sleep(2)
+            return redirect(url_for('index'))
+        else:
+            flash('Email ou senha inválido!')
+
+    return render_template('login.html', titulo = 'Login')
+
 @app.route('/sobre')
 def sobre():
     return render_template('sobre.html',titulo = 'Sobre')
@@ -33,6 +72,5 @@ def sobre():
 def produtos():
     return render_template('produto.html',titulo = 'Produtos')
 
-@app.route('/teste')
-def teste():
-    return render_template('teste.html',titulo = 'Teste')
+
+
