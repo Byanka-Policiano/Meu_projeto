9255b6e99fec5aa901ff944ecdfc44fbb0f6c440
Byanka Policiano Santos <policianobyanka@gmail.com>
2023-08-22 11:44:10 -0300

    22/08/2023-11:44

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
-from app import app, db
+from app import app, db, bcrypt
 from flask import render_template, url_for, request, flash, session, redirect
 from app.forms import Contato
 from app.models import ContatoModel
 from app.forms import Cadastro
 from app.models import CadastroModel
+from flask_bcrypt import check_password_hash
 import time 
 
 
@@ -32,37 +33,50 @@ def contatos():
 @app.route('/cadastro', methods=['POST', 'GET'])
 def cadastro():
     cadastro = Cadastro()
-    print('Acessou a rota de cadastro')
+    print('Acessou a rota de cadastro!')
     if cadastro.validate_on_submit():
         flash('Seu cadastro foi realizado com sucesso!')
-
-        nome = cadastro.nome.data 
+        nome = cadastro.nome.data
         sobrenome = cadastro.sobrenome.data
         email = cadastro.email.data
         senha = cadastro.senha.data
-
-        novo_cadastro = CadastroModel(nome=nome, sobrenome=sobrenome, email=email,  senha=senha)
+        hash_senha = bcrypt.generate_password_hash(senha).decode('utf-8')
+        novo_cadastro = CadastroModel(nome = nome, sobrenome=sobrenome, email=email, senha=hash_senha)
         db.session.add(novo_cadastro)
-        db.session.commit()
+        db.session.commit() 
 
-
-    return render_template('cadastro.html', titulo = 'Cadastro', cadastro=cadastro)
+    return render_template('cadastro.html', tituto = 'Cadastro',cadastro = cadastro)
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
         email = request.form.get('email').lower()
-        senha = request.form.get('senha')
+        senha = request.form.get('senha') 
+        usuario = CadastroModel.query.filter_by(email=email).first()
 
-        usuario = CadastroModel.query.filter_by(email = email, senha = senha).first()
-        if usuario and usuario.senha == senha:
-            session['email'] = usuario.id
-            time.sleep(2)
-            return redirect(url_for('index'))
+        print("Email digitado:", email)
+        print("Senha digitada:", senha)
+        
+        if usuario:
+            print("Hash de senha no banco de dados:", usuario.senha)
+            if bcrypt.check_password_hash(usuario.senha, senha):
+                print("Senha válida. Efetuando login...")
+                session['email'] = usuario.email
+                session['nome'] = usuario.nome
+                session['sobrenome'] = usuario.sobrenome
+                time.sleep(2)
+                return redirect(url_for('index'))
+            else:
+                print("Senha incorreta.")
         else:
-            flash('Email ou senha inválido!')
+            print("Usuário não encontrado.")
+            flash('E-mail ou senha inválido!')
 
-    return render_template('login.html', titulo = 'Login')
+    return render_template('login.html', titulo='Login')
+
+
+
+
 
 @app.route('/sobre')
 def sobre():
@@ -72,5 +86,32 @@ def sobre():
 def produtos():
     return render_template('produto.html',titulo = 'Produtos')
 
+@app.route('/editar', methods={'POST','GET'})
+def editar():
+    if 'email' not in session:
+        return redirect(url_for('login'))
+    
+    usuario = CadastroModel.query.filter_by(email = session['email']).first()
+    if request.method == 'POST':
+        usuario.nome = request.form.get('nome')
+        usuario.sobrenome = request.form.get('sobrenome')
+        usuario.email = request.form.get('email')
+        senha = request.form.get('senha')
+        usuario.senha = bcrypt.generate_password_hash(senha).decode('utf-8')
+        db.session.commit() 
+        session['email'] = usuario.email
+        session['nome'] = usuario.nome
+        session['sobrenome'] = usuario.sobrenome
+        session['senha'] = usuario.senha
+        flash('Seus dados foram atualizados com sucesso!')
+
+    return render_template('editar.html',titulo = 'Editar')
 
 
+@app.route('/sair')
+def sair():
+     session['email'] = None
+     session['nome'] = None
+     session['sobrenome'] = None
+     session['senha'] = None
+     return redirect(url_for('login'))
